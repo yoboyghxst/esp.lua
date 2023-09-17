3de5847837d398fd02d125273e20e78a74f0df85
yoboyghxst <143840193+yoboyghxst@users.noreply.github.com>
2023-09-17 09:24:44 +0200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,13 +1,15 @@
 local Players = game:GetService("Players")
-	
 local function highlightPlayer(player)
-	local highlight = Instance.new("Highlight")
-	highlight.Parent = player.Character
-	highlight.FillColor = Color3.fromRGB(255, 255, 255)
-	highlight.OutlineColor = Color3.fromRGB(255, 0, 0)
-end
-	
 
-for _, player in ipairs(Players:GetPlayer()) do
-	highlightPlayer(player)
+  if player.Character then
+    
+    local highlight = Instance.new("Highlight")
+    highlight.Parent = player.Character
+    highlight.FillColor = Color3.fromRGB(255, 255, 255)
+    highlight.OutlineColor = Color3.fromRGB(255, 0, 0)
+  end
+end
+
+for _, player in ipairs(Players:GetPlayers()) do
+  highlightPlayer(player)
 end
